e8344288e6256803451c2501ba37aa1e73fe8646
jmc27 <jmc27@brandeis.edu>
2014-02-04 11:07:40 -0500

    Time test change

--- Test.rb
+++ Test.rb
@@ -1,8 +1,12 @@
 require_relative "MovieData"
 class Test
+	start = Time.now
 	test = MovieData.new("ml-100k/u1.base")
 	test.run_test
-	puts test.testdata.meanerror
+	puts test.testdata.mean
 	puts test.testdata.stddev
 	puts test.testdata.rmsd
+	finish = Time.now
+	time = finish - start
+	puts "Time: " + time.to_s
 end
